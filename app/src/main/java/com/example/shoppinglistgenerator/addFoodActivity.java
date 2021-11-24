6205a9cdb8d81d7484928c81c610a50bf566e7e4
ndi1 <jhelton773@gmail.com>
2021-11-23 22:02:54 -0500

    Modified addFood activity

--- app/src/main/java/com/example/shoppinglistgenerator/addFoodActivity.java
+++ app/src/main/java/com/example/shoppinglistgenerator/addFoodActivity.java
@@ -3,12 +3,21 @@ package com.example.shoppinglistgenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class addFoodActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food);
+        Spinner servingsSpinner = findViewById(R.id.servingSpinner);
+        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this,R.array.servings, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        servingsSpinner.setAdapter(adapter);
+
     }
 }
