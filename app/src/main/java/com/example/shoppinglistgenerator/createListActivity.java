3ecb984f68cc88c03065edde132add332e0b4534
ndi1 <jhelton773@gmail.com>
2021-12-01 20:21:21 -0500

    Modified Recipe Fragment

--- app/src/main/java/com/example/shoppinglistgenerator/createListActivity.java
+++ app/src/main/java/com/example/shoppinglistgenerator/createListActivity.java
@@ -3,6 +3,12 @@ package com.example.shoppinglistgenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class createListActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class createListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_list);
+
+        //Testing ListView
+        final ListView shoppingListView = (ListView) findViewById(R.id.shoppingListView);
+        String[] items = new String[]{
+                "Item 1",
+                "Item 2",
+        };
+
+        final List<String> shopping_list = new ArrayList<String>(Arrays.asList(items));
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,shopping_list);
+        shoppingListView.setAdapter(arrayAdapter);
+//Testing ListView
+
+
     }
 }
