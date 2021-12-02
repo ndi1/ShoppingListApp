9e5476bc95712096b19867d935d472ee3b28d807
ndi1 <jhelton773@gmail.com>
2021-12-01 19:52:19 -0500

    Modified Recipe Fragment

--- app/src/main/java/com/example/shoppinglistgenerator/recipeIngredientFragment.java
+++ app/src/main/java/com/example/shoppinglistgenerator/recipeIngredientFragment.java
@@ -1,30 +1,36 @@
 package com.example.shoppinglistgenerator;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class recipeIngredientFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
     public recipeIngredientFragment() {
-        // Required empty public constructor
+
     }
 
-    // TODO: Rename and change types and number of parameters
+
     public static recipeIngredientFragment newInstance(String param1, String param2) {
         recipeIngredientFragment fragment = new recipeIngredientFragment();
         Bundle args = new Bundle();
@@ -40,13 +46,32 @@ public class recipeIngredientFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_ingredient, container, false);
+        View view = inflater.inflate(R.layout.fragment_recipe_ingredient, container, false);
+
+
+        //Testing ListView
+        ListView lv = (ListView) view.findViewById (R.id.recipeIngredientListView);
+        String[] items = new String[]{
+                "Item 1",
+                "Item 2",
+        };
+        List<String> item_list = new ArrayList<String>(Arrays.asList(items));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1,items
+        );
+        lv.setAdapter(arrayAdapter);
+
+        return view;
+
+//Testing ListView
     }
 }
