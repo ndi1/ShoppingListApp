beec28d99aa41cf0ba28786e30cb770d01d107bd
ndi1 <jhelton773@gmail.com>
2022-01-02 17:02:45 -0500

    Added additional screens for overlay and recipes

--- app/src/main/java/com/example/shoppinglistgenerator/addRecipeActivitiy.java
+++ app/src/main/java/com/example/shoppinglistgenerator/addRecipeActivitiy.java
@@ -2,14 +2,35 @@ package com.example.shoppinglistgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class addRecipeActivitiy extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         setContentView(R.layout.activity_add_recipe_activitiy);
+        floatingActionButton = findViewById(R.id.addFoodToRecipeFAB);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFoodSearch();
+            }
+        });
+
+
     }
+
+    public void openFoodSearch(){
+        Intent intent = new Intent(this,foodSearchActivity.class);
+        startActivity(intent);
+    }
+
 }
